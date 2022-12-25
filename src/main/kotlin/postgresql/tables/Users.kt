6630e422105185677c9c8db697e85e6c9b512add
Tim Korelov <timkorelov@gmail.com>
2022-12-26 00:41:28 +0300

    Added HikariCP dependency

--- src/main/kotlin/postgresql/tables/Users.kt
+++ src/main/kotlin/postgresql/tables/Users.kt
@@ -9,10 +9,10 @@ import org.jetbrains.exposed.sql.kotlin.datetime.date
  */
 object Users : Table("users") {
     val id = integer("id").autoIncrement()
-    val name = text("name")
+    val name = text("name").nullable()
     val user_id = integer("user_id")
-    val telegram_id = text("telegram_id")
-    val birth_date = date("birth_date")
+    val telegram_id = text("telegram_id").nullable()
+    val birth_date = date("birth_date").nullable()
     val locale = varchar("locale", 2)
     val time_zone = integer("time_zone")
     override val primaryKey = PrimaryKey(id, name = "users_pkey")
